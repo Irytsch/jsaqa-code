9abfa66605e746271471ef31f4db8ea3c05ec24f
Irina Khvatova <i.chwatowa@mail.ru>
2023-08-23 20:58:09 +0300

    Init commi

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -1,17 +1,37 @@
-const sorting = require("../../app");
+const sorting = require('../../app');
 
-describe("Books names test suit", () => {
-  it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
-      "Властелин Колец",
-      "Волшебник изумрудного города",
-      "Гарри Поттер",
-    ]);
+describe('Books names test suit', () => {
+  it('Books names should be sorted in ascending order', () => {
+    const input = [
+      'Гарри Поттер',
+      'Властелин Колец',
+      'Волшебник изумрудного города',
+    ];
+    const expected = [
+      'Властелин Колец',
+      'Волшебник изумрудного города',
+      'Гарри Поттер',
+    ];
+
+    const result = sorting.sortByName(input);
+
+    expect(result).toEqual(expected);
+  });
+
+  test('Book names should not be sorted', () => {
+    const input = [
+      'Три товарища',
+      'Гордость и предубеждение',
+      'Гордость и предубеждение',
+    ];
+    const expected = [
+      'Гордость и предубеждение',
+      'Гордость и предубеждение',
+      'Три товарища',
+    ];
+
+    const result = sorting.sortByName(input);
+
+    expect(result).toEqual(expected);
   });
 });
